618939aa375a69302ca7c96b892aa415c7b6e84a
Daniel Gauci <daniel.gauci@gmail.com>
2026-09-09 13:42:20 +0200

    feat(api): api update (#513)
    Co-authored-by: roark-codegen <codegen@roark.ai>

--- src/roark_analytics/types/config_flow_step.py
+++ src/roark_analytics/types/config_flow_step.py
@@ -15,6 +15,7 @@ class ConfigFlowStep(BaseModel):
         "AGENT_TURN",
         "CUSTOMER_TURN",
         "CUSTOMER_FIRST_MESSAGE",
+        "CUSTOMER_VERBATIM_TURN",
         "CUSTOMER_SILENCE",
         "CUSTOMER_DTMF",
         "AGENT_DTMF",
